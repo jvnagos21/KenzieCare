b873f6c989b3cb2acf3e7549ad9a4ec9ca626674
ygor rupp <ygor.rupp@hotmail.com>
2023-01-09 10:39:58 -0300

    feat: finish css

--- kenziecare/src/Pages/AdminDash/index.tsx
+++ kenziecare/src/Pages/AdminDash/index.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from "react";
+import { HeaderAdmin } from "../../Components/HeaderAdmin";
+import { ContainerAdminDash } from "./style";
 
 const AdminDash = () => {
   return (
-    <div>AdminDash</div>
-  )
-}
+    <div>
+      <HeaderAdmin>
+        <div>
+          <img src="/Logo.png" alt="iconLogo" />
+          <button>Logout</button>
+        </div>
+      </HeaderAdmin>
+      <ContainerAdminDash>
+        <ul>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+            <li>
+               <img src="/heading1.svg" alt="" />
+               <h2>nome da ong</h2>
+               <p>Estado da ong</p>
+               <span>Area de atuação</span>
+               <div>
+                  <button>Edit</button>
+               </div>
+            </li>
+          </ul>
+      </ContainerAdminDash>
+    </div>
+  );
+};
 
-export default AdminDash
+export default AdminDash;
